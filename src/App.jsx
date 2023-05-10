58df834dccb64bd872f4e34c26faa41e38217d7f
fiverrdev19 <fiverrdev19@gmail.com>
2023-05-10 15:34:04 +0800

    Template added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+import { Layout } from "./components/Layout.jsx";
+import { AcmeLogo } from "./components/AcmeLogo.jsx";
+import { VariantsSelectorWrapper } from "./components/VariantsSelectorWrapper.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [variant, setVariant] = React.useState("static");
 
+  const variants = ["static", "floating", "sticky"];
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout>
+      <Navbar isBordered variant={variant}>
+        <Navbar.Brand>
+          <AcmeLogo />
+          <Text b color="inherit" hideIn="xs">
+            ACME
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content hideIn="xs">
+          <Navbar.Link href="#">Features</Navbar.Link>
+          <Navbar.Link isActive href="#">Customers</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Company</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="#">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat as={Link} href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+      <VariantsSelectorWrapper>
+        <Card css={{maxW: "50%"}}>
+          <Card.Body css={{pt: "$8", px: "$8"}}>
+            <Radio.Group
+              defaultValue="default"
+              label="Select variant"
+              orientation="horizontal"
+              size="sm"
+              value={variant}
+              onChange={setVariant}
+            >
+              {variants.map((variant) => (
+                <Radio key={variant} value={variant}>
+                  {variant}
+                </Radio>
+              ))}
+            </Radio.Group>
+          </Card.Body>
+        </Card>
+      </VariantsSelectorWrapper>      
+    </Layout>
   )
 }
-
-export default App
